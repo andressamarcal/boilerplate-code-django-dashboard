f9729ab3c8cccfd6f3b042d1074654583de7af5b
Adi Chirilov <developer@rosoftware.ro>
2020-02-08 07:36:45 +0200

    Dummy

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'boilerplate-django-dashboard.herokuapp.com']
 
 # Application definition
 
