a897a9bdfd2a363dfd5dd94b8154f448fb573626
Adi Chirilov <developer@rosoftware.ro>
2020-02-08 09:07:05 +0200

    Update Static Assets management

--- app/views.py
+++ app/views.py
@@ -9,6 +9,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
 
+from django.shortcuts import render
+from django.template import RequestContext
+
 @login_required(login_url="/login/")
 def index(request):
     return render(request, "index.html")
@@ -28,3 +31,14 @@ def pages(request):
 
         template = loader.get_template( 'pages/error-404.html' )
         return HttpResponse(template.render(context, request))
+
+def handler404(request, *args, **argv):
+    response = render(request, 'pages/error-404.html', {})
+    response.status_code = 404
+    return response
+
+
+def handler500(request, *args, **argv):
+    response = render(request, 'pages/error-500.html', {})
+    response.status_code = 500
+    return response
